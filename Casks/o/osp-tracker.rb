4cad54aed3fa2981c801eaca3118116cbeef1991
Sam Ford <1584702+samford@users.noreply.github.com>
2026-03-30 01:01:03 -0400

    osp-tracker 6.3.4
    `osp-tracker` is autobumped but the workflow failed to update to
    version 6.3.4 because physlets.org was inaccessible at the time, so
    this manually updates the version.

--- Casks/o/osp-tracker.rb
+++ Casks/o/osp-tracker.rb
@@ -1,6 +1,6 @@
 cask "osp-tracker" do
-  version "6.3.3"
-  sha256 "fbd43076ffca63ee6691b9ef175cc792b939d2e2182dd597bc2389a367f8e228"
+  version "6.3.4"
+  sha256 "6cbb7e26db6f7fb38315383f09755181fa685a110a5942db35179b9e59a1d5d2"
 
   url "https://physlets.org/tracker/installers/Tracker-#{version}-osx-installer.dmg",
       verified: "physlets.org/tracker/installers/"
